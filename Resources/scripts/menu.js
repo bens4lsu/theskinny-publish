60e7868f6b09663b826b23f2d7c59633ef45c7b1
Ben Schultz <bens4lsu@gmail.com>
2025-11-20 14:28:39 -0600

    fix a menu bug and a misspelled word.

--- Resources/scripts/menu.js
+++ Resources/scripts/menu.js
@@ -13,11 +13,12 @@ $(document).ready(function() {
     $("li.menu-collapser:first-child a").click(function() {
         var collapser = $(this).parent();
         collapser.children("ul").toggleVisibility();
-        if (collapser.children(".li-pagelink:visible").length > 0) {
-            $(this).children(".span-collapser").text(" ▼︎");
+        console.log(collapser.find(".li-pagelink:visible").length);
+        if (collapser.find(".li-pagelink:visible").length > 0) {
+            $(this).find(".span-collapser").text(" ▼︎");
             $(this).css("text-decoration", "underline");
         } else {
-            $(this).children(".span-collapser").text(" ▶");
+            $(this).find(".span-collapser").text(" ▶");
             $(this).css("text-decoration", "none");
         }
     }); 
